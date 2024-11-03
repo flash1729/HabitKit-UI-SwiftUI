0c074d244792ddfe314dc703e456e991d77cf2a9
flash1729 <sherlockedaditya@gmail.com>
2024-11-03 10:46:56 +0530

    Add main app file

---
+++ HabitKitUI/HabitKitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitlKit_UIApp.swift
+//  HabitlKit UI
+//
+//  Created by Aditya Medhane on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitlKit_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
